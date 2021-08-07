f6eb96dc9fbe71dfbff43893b0c86a5f01caac6a
Iramar Falcao <iramar.j@hotmail.com>
2021-08-07 10:40:25 -0300

    Setting a button function.

--- app/src/main/java/com/iramarfalcao/aboutme/MainActivity.kt
+++ app/src/main/java/com/iramarfalcao/aboutme/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.iramarfalcao.aboutme
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.done_button).setOnClickListener {
+            addNickname(it)
+        }
+    }
+
+    private fun addNickname(view: View) {
+        val nicknameEditText: EditText = findViewById(R.id.nickname_edit)
+        val nicknameText: TextView = findViewById(R.id.nickname_text)
+
+        nicknameText.text = nicknameEditText.text
+        nicknameEditText.visibility = View.GONE
+        view.visibility = View.GONE
+        nicknameText.visibility = View.VISIBLE
+
+        // Hide the keyboard.
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
